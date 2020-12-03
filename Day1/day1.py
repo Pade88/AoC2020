820bd9faf50dda13f37988ea874f3ed2c1009761
Pade88 <45396707+Pade88@users.noreply.github.com>
2020-12-03 19:36:46 +0200

    Add files via upload

---
+++ Day1/day1.py
@@ -0,0 +1,65 @@
+from itertools import product
+my_inputs = list(open("input.txt", "r").read().split('\n'))
+
+
+def check(input_1 = 0, input_2 = 0, input_3 =0):
+	return True if (input_1 + input_2 + input_3 == 2020) else False
+
+def function(inputs):
+	loop_counter = 1
+	for first_loop in inputs:
+		for second_loop in inputs[loop_counter+1:]:
+			for third_loop in inputs[loop_counter+2:]:
+				if check(int(first_loop), int(second_loop), int(third_loop)):
+					return int(first_loop) * int(second_loop) * int(third_loop)
+		loop_counter = loop_counter + 1
+
+
+if __name__ =="__main__":
+	print(function(my_inputs))
+
+"""
+--- Day 1: Report Repair ---
+After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
+
+The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+
+To save your vacation, you need to get all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+
+Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+
+For example, suppose your expense report contained the following:
+
+1721
+979
+366
+299
+675
+1456
+In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+Your puzzle answer was 224436.
+
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+
+Your puzzle answer was 303394260.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
+At this point, you should return to your Advent calendar and try another puzzle.
+
+If you still want to see it, you can get your puzzle input.
+
+You can also [Share] this puzzle.
+""
